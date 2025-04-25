ca36caaf14efb36d02ef54fd5b8722111fa7f018
jpmsilvamessias <joaopedr901673@gmail.com>
2025-04-25 01:40:10 -0300

    update-criando a classe Produto

--- src/model/entites/Product.java
+++ src/model/entites/Product.java
@@ -1,4 +1,59 @@
 package model.entites;
 
+import model.enums.Types;
+
 public class Product {
+    String name;
+    Types category;
+    Integer id;
+    Integer quantity;
+    Integer price;
+
+    public Product(String name, Types category, Integer id, Integer quantity, Integer price) {
+        this.name = name;
+        this.category = category;
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Types getCategory() {
+        return category;
+    }
+
+    public void setCategory(Types category) {
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
