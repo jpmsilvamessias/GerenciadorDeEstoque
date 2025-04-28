717a4e1bcda3699ac30a5c32fd90fef0bc03337d
jpmsilvamessias <joaopedr901673@gmail.com>
2025-04-28 01:45:21 -0300

    update-criando o gerenciador de estoque

--- src/model/app/Programing.java
+++ src/model/app/Programing.java
@@ -1,9 +1,15 @@
 package model.app;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Programing {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Seja bem vindo ao sistema de cadastro de produtos");
+        int decisao=0;
+
     }
 }
